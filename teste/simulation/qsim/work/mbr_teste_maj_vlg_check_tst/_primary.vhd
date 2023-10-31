787deebef0cfe52320162ab7707ea90f5af24209
Maximus Rosa <maxborgesrosa@gmail.com>
2023-10-30 22:49:14 -0300

    removendo arquivos desnecessários

--- teste/simulation/qsim/work/mbr_teste_maj_vlg_check_tst/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity mbr_teste_maj_vlg_check_tst is
-    port(
-        s               : in     vl_logic;
-        sampler_rx      : in     vl_logic
-    );
-end mbr_teste_maj_vlg_check_tst;
--- teste/simulation/qsim/work/mbr_teste_maj_vlg_vec_tst/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity mbr_teste_maj_vlg_vec_tst is
-end mbr_teste_maj_vlg_vec_tst;
