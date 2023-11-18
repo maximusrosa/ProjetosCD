05d4084d25ffe7546589d25c4c3d077170a8d9ba
Maximus Rosa <maxborgesrosa@gmail.com>
2023-11-17 23:34:18 -0300

    teste
    testando o github desktop

--- TP5/simulation/qsim/work/mbr_div_freq_1b/_primary.vhd
+++ TP5/simulation/qsim/work/mbr_div_freq_1b/_primary.vhd
@@ -3,8 +3,8 @@ use verilog.vl_types.all;
 entity mbr_div_freq_1b is
     port(
         Q               : out    vl_logic;
-        D               : out    vl_logic;
         RST             : in     vl_logic;
-        CK              : in     vl_logic
+        CLK             : in     vl_logic;
+        D               : out    vl_logic
     );
 end mbr_div_freq_1b;
--- TP5/simulation/qsim/work/mbr_div_freq_1b_vlg_sample_tst/_primary.vhd
+++ TP5/simulation/qsim/work/mbr_div_freq_1b_vlg_sample_tst/_primary.vhd
@@ -2,7 +2,7 @@ library verilog;
 use verilog.vl_types.all;
 entity mbr_div_freq_1b_vlg_sample_tst is
     port(
-        CK              : in     vl_logic;
+        CLK             : in     vl_logic;
         RST             : in     vl_logic;
         sampler_tx      : out    vl_logic
     );
